376966e256c5b200a0d32a89f312493c9dd65fc0
jqakubx <kuberserk@gmail.com>
2022-03-12 11:35:41 +0100

    #8 add basic field
    # add simple field 'plain' with texture and 3d model
    # simple display in the world this model

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -22,61 +23,71 @@ import lombok.NonNull;
 
 public class MyGdxGame extends ApplicationAdapter {
 
-	CompositeUpdatable compositeUpdatable = new CompositeUpdatable();
-	Viewport viewport;
-	ModelBatch batch;
-	Texture img;
-	ModelInstance modelInstance;
+    CompositeUpdatable compositeUpdatable = new CompositeUpdatable();
+    Viewport viewport;
+    ModelBatch batch;
+    Texture img;
+    ModelInstance modelInstance;
+    ModelInstance fieldInstance;
+    AssetManager assets;
+    Model field;
 
-	private @NonNull Viewport createViewport() {
-		Camera camera = new OrthographicCamera(300, 300);
-		camera.near = 1f;
-		camera.far = 300f;
-		return new ExtendViewport(300, 300, camera);
-	}
+    private @NonNull Viewport createViewport() {
+        Camera camera = new OrthographicCamera(300, 300);
+        camera.near = 1f;
+        camera.far = 300f;
+        return new ExtendViewport(300, 300, camera);
+    }
 
-	private @NonNull ModelInstance createModelInstance() {
-		Model model = new ModelBuilder().createBox(25, 25, 25,
-				new Material(TextureAttribute.createDiffuse(img)),
-				VertexAttributes.Usage.Position | VertexAttributes.Usage.TextureCoordinates);
-		return new ModelInstance(model, new Vector3(0, 0, 0));
-	}
+    private @NonNull ModelInstance createModelInstance() {
+        Model model = new ModelBuilder().createBox(25, 25, 25,
+                new Material(TextureAttribute.createDiffuse(img)),
+                VertexAttributes.Usage.Position | VertexAttributes.Usage.TextureCoordinates);
+        return new ModelInstance(model, new Vector3(0, 0, 0));
+    }
 
-	@Override
-	public void create () {
-		viewport = createViewport();
+    @Override
+    public void create() {
+        viewport = createViewport();
 
-		viewport.getCamera().position.set(0, 100, 100);
-		viewport.getCamera().lookAt(0, 0, 0);
+        viewport.getCamera().position.set(0, 100, 100);
+        viewport.getCamera().lookAt(-100, 0, 0);
 
-		CameraMoverInputProcessor inputProcessor = new CameraMoverInputProcessor(viewport);
-		compositeUpdatable.addUpdatable(inputProcessor.getCameraControl());
-		Gdx.input.setInputProcessor(inputProcessor);
+        CameraMoverInputProcessor inputProcessor = new CameraMoverInputProcessor(viewport);
+        compositeUpdatable.addUpdatable(inputProcessor.getCameraControl());
+        Gdx.input.setInputProcessor(inputProcessor);
 
-		img = new Texture("badlogic.jpg");
-		modelInstance = createModelInstance();
+        img = new Texture("badlogic.jpg");
+        assets = new AssetManager();
+        assets.load("field.g3db", Model.class);
+        assets.finishLoading();
+        field = assets.get("field.g3db", Model.class);
+        modelInstance = createModelInstance();
+        fieldInstance = new ModelInstance(field, new Vector3(0, 0, 0));
 
-		batch = new ModelBatch();
-	}
+        batch = new ModelBatch();
+    }
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(0.5f, 0.5f, 0.5f, 1, true);
-		compositeUpdatable.update();
-		viewport.getCamera().update();
-		batch.begin(viewport.getCamera());
-		batch.render(modelInstance);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void render() {
+        ScreenUtils.clear(0.5f, 0.5f, 0.5f, 1, true);
+        compositeUpdatable.update();
+        viewport.getCamera().update();
+        batch.begin(viewport.getCamera());
+        batch.render(modelInstance);
+        batch.render(fieldInstance);
+        batch.end();
+    }
 
-	@Override
-	public void resize(int width, int height) {
-		viewport.update(width, height);
-	}
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+        field.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
 }
