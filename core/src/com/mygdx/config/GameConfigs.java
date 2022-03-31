eec372fc3a11fa8a038a03ab2f547f916c19fba4
Kacper Jankowski <kjankowski@student.agh.edu.pl>
2022-03-31 19:45:34 +0200

    #8 Refactor GameConfigs

--- core/src/com/mygdx/config/GameConfigs.java
+++ core/src/com/mygdx/config/GameConfigs.java
@@ -9,43 +9,48 @@ import java.util.Map;
 public class GameConfigs {
 
     @NonNull
-    private final Map<Class<? extends EntityConfig>, Map<String, EntityConfig>> entityConfigListMap;
+    private final Map<Class<? extends EntityConfig>, Map<String, EntityConfig>> entityConfigMap;
 
     public GameConfigs() {
-        entityConfigListMap = new HashMap<>();
+        entityConfigMap = new HashMap<>();
     }
 
-    public <T extends EntityConfig> T get(@NonNull final Class<T> fieldClass, @NonNull final String entityName) {
-        return fieldClass.cast(entityConfigListMap.get(fieldClass).get(entityName));
+    public <T extends EntityConfig> T get(@NonNull final Class<T> entityClass,
+                                          @NonNull final String entityName) {
+        return entityClass.cast(entityConfigMap.get(entityClass).get(entityName));
     }
 
     @NonNull
-    public <T extends EntityConfig> T getAny(@NonNull final Class<T> fieldClass) {
-        return fieldClass.cast(entityConfigListMap.get(fieldClass).values()
+    public <T extends EntityConfig> T getAny(@NonNull final Class<T> entityClass) {
+        return entityClass.cast(entityConfigMap.get(entityClass).values()
                 .stream()
                 .findAny().orElseThrow());
     }
 
-    public <T extends EntityConfig> void put(@NonNull final Class<T> clazz, @NonNull final T entityConfig) {
+    public <T extends EntityConfig> void put(@NonNull final Class<T> entityClass,
+                                             @NonNull final T entityConfig) {
         var singleItemArray = new Array<T>(1);
         singleItemArray.add(entityConfig);
-        putAll(clazz, singleItemArray);
+        putAll(entityClass, singleItemArray);
     }
 
-    public <T extends EntityConfig> void putAll(Class<T> clazz, Array<T> array) {
-        if (entityConfigListMap.containsKey(clazz)) {
-            var entityMap = entityConfigListMap.get(clazz);
-            for (var i = 0; i < array.size; i++) {
-                var entityConfig = array.get(i);
-                entityMap.put(entityConfig.getName(), entityConfig);
-            }
+    public <T extends EntityConfig> void putAll(@NonNull final Class<T> entityClass,
+                                                @NonNull final Array<T> entityArray) {
+        if (entityConfigMap.containsKey(entityClass)) {
+            var entityMap = entityConfigMap.get(entityClass);
+            putEntityConfigsInMap(entityMap, entityArray);
         } else {
             var entityMap = new HashMap<String, EntityConfig>();
-            for (var i = 0; i < array.size; i++) {
-                var entityConfig = array.get(i);
-                entityMap.put(entityConfig.getName(), entityConfig);
-            }
-            entityConfigListMap.put(clazz, entityMap);
+            putEntityConfigsInMap(entityMap, entityArray);
+            entityConfigMap.put(entityClass, entityMap);
+        }
+    }
+
+    private <T extends EntityConfig> void putEntityConfigsInMap(Map<String, EntityConfig> entityMap,
+                                                                Array<T> entityArray) {
+        for (var i = 0; i < entityArray.size; i++) {
+            var entityConfig = entityArray.get(i);
+            entityMap.put(entityConfig.getName(), entityConfig);
         }
     }
 }
