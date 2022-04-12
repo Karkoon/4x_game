882fd2b71a61beee36d0ae9a74789aaceac76094
Kacper Jankowski <kjankowski@student.agh.edu.pl>
2022-04-12 21:29:47 +0200

    #10 Implement Poolable in PositionComponent

--- core/src/com/mygdx/game/client/component/PositionComponent.java
+++ core/src/com/mygdx/game/client/component/PositionComponent.java
@@ -2,12 +2,17 @@ package com.mygdx.game.client.component;
 
 import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Pool;
 import lombok.Data;
 import lombok.NonNull;
 
 @Data
-public class PositionComponent implements Component {
+public class PositionComponent implements Component, Pool.Poolable {
 
   private @NonNull Vector3 position = new Vector3();
 
+  @Override
+  public void reset() {
+    position.set(Vector3.Zero);
+  }
 }
