1fc17601086138846d88c50c23a4b0d27e18dde0
Kacper Jankowski <kjankowski@student.agh.edu.pl>
2022-04-12 19:52:10 +0200

    #10 Add TEXTURE_DIR

--- core/src/com/mygdx/game/assets/AssetPaths.java
+++ core/src/com/mygdx/game/assets/AssetPaths.java
@@ -8,6 +8,7 @@ public final class AssetPaths {
   public static final String ENTITY_CONFIG_DIR = ASSETS_PATH + "entity_configs/";
   public static final String FIELD_CONFIG_DIR = ENTITY_CONFIG_DIR + "fields/";
   public static final String UNIT_CONFIG_DIR = ENTITY_CONFIG_DIR + "units/";
+  public static final String TEXTURE_DIR = MODEL_DIR;
 
   private AssetPaths() {
   }
