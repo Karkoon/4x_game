39e9268d57b18a793cb4ada7509131ee26cce574
Jakub Klimczak <kuberserk@gmail.com>
2022-04-13 19:56:02 +0200

    #7 Fix camera

--- core/src/com/mygdx/game/client/screen/GameScreen.java
+++ core/src/com/mygdx/game/client/screen/GameScreen.java
@@ -56,8 +56,8 @@ public class GameScreen extends ScreenAdapter {
   }
 
   private void positionCamera(@NonNull Camera camera) {
-    camera.position.set(0, 500, 0);
-    camera.lookAt(0, -100, 0);
+    camera.position.set(0, 300, 0);
+    camera.lookAt(0, 0, 0);
   }
 
   @Override
