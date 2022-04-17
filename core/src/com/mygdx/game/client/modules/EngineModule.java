85e2b0a2e012c91c8f30639ba649c6bd92c85734
Kacper Jankowski <kjankowski@student.agh.edu.pl>
2022-04-17 23:48:23 +0200

    #18 Add logging to Modules

--- core/src/com/mygdx/game/client/modules/EngineModule.java
+++ core/src/com/mygdx/game/client/modules/EngineModule.java
@@ -5,14 +5,18 @@ import com.badlogic.ashley.core.PooledEngine;
 import dagger.Module;
 import dagger.Provides;
 import lombok.NonNull;
+import lombok.extern.java.Log;
 
 import javax.inject.Singleton;
+import java.util.logging.Level;
 
 @Module
+@Log
 public class EngineModule {
   @Provides
   @Singleton
   public @NonNull Engine providesEngine() {
+    log.log(Level.INFO, "provided Engine");
     return new PooledEngine();
   }
 }
