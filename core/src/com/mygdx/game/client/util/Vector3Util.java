c0d138f415a45dd079ac0469f0f612f737c421db
Kacper Jankowski <kjankowski@student.agh.edu.pl>
2022-04-25 19:49:56 +0200

    #18 Add null checks to Vector3Util

--- core/src/com/mygdx/game/client/util/Vector3Util.java
+++ core/src/com/mygdx/game/client/util/Vector3Util.java
@@ -2,10 +2,12 @@ package com.mygdx.game.client.util;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import lombok.NonNull;
 
 public final class Vector3Util {
 
-  public static Vector2 toVector2(Vector3 vec3) {
+  @NonNull
+  public static Vector2 toVector2(@NonNull Vector3 vec3) {
     return new Vector2(vec3.x, vec3.y);
   }
 
