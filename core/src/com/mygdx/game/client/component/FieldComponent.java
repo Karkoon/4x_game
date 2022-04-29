59f8dd265017bca123c6c8adb28b7551d92e69ec
Jakub Klimczak <kuberserk@gmail.com>
2022-04-29 15:26:25 +0200

    #43 add field component
    * add field component to store unit and coords

---
+++ core/src/com/mygdx/game/client/component/FieldComponent.java
@@ -0,0 +1,17 @@
+package com.mygdx.game.client.component;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.Entity;
+import com.mygdx.game.client.model.Coordinates;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@NoArgsConstructor
+public class FieldComponent implements Component {
+
+  private Entity unitEntity;
+  @NonNull
+  private Coordinates coordinates;
+}
