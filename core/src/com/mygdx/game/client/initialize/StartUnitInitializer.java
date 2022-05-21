7bbed3fdb96b85117fba26b59ed46eb116a0d39f
Kacper Jankowski <kjankowski@student.agh.edu.pl>
2022-05-21 23:02:19 +0200

    #51 Make StartUnitInitializer an injectable object

--- core/src/com/mygdx/game/client/initialize/StartUnitInitializer.java
+++ core/src/com/mygdx/game/client/initialize/StartUnitInitializer.java
@@ -3,23 +3,32 @@ package com.mygdx.game.client.initialize;
 import com.badlogic.ashley.core.Entity;
 import com.mygdx.game.assets.GameScreenAssets;
 import com.mygdx.game.client.ecs.component.Slot;
-import com.mygdx.game.client.ecs.component.UnitMovement;
 import com.mygdx.game.client.ecs.entityfactory.UnitFactory;
 import com.mygdx.game.client.model.Coordinates;
 import com.mygdx.game.config.UnitConfig;
+import lombok.NonNull;
 
+import javax.inject.Inject;
 import java.util.Map;
 
 public final class StartUnitInitializer {
 
-  private StartUnitInitializer() {
+  private final UnitFactory unitFactory;
+  private final GameScreenAssets assets;
 
+  @Inject
+  public StartUnitInitializer(@NonNull UnitFactory unitFactory,
+                              @NonNull GameScreenAssets assets) {
+    this.unitFactory = unitFactory;
+    this.assets = assets;
   }
 
-  public static void initializeTestUnit(UnitFactory unitFactory, GameScreenAssets assets, Map<Coordinates, Entity> fieldList) {
+  public void initializeTestUnit(Map<Coordinates, Entity> fieldList) {
     var initialCoordinates = new Coordinates(0, 0);
-    var unitEntity = unitFactory.createEntity(assets.getGameConfigs().getAny(UnitConfig.class), initialCoordinates);
-    fieldList.get(initialCoordinates).getComponent(Slot.class).setUnitEntity(unitEntity);
-    unitEntity.getComponent(UnitMovement.class).setFromAndTo(fieldList.get(initialCoordinates), fieldList.get(initialCoordinates));
+    var anyConfig = assets.getGameConfigs().getAny(UnitConfig.class);
+    var unitEntity = unitFactory.createEntity(anyConfig, initialCoordinates);
+
+    var slot = fieldList.get(initialCoordinates).getComponent(Slot.class);
+    slot.getEntities().add(unitEntity);
   }
 }
