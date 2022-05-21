f654234e33de6772b2b0fb4edaeb40da4c857f05
Kacper Jankowski <kjankowski@student.agh.edu.pl>
2022-05-21 23:04:52 +0200

    #51 Make MapInitializer an injectable object. Remove InitialParameters

--- core/src/com/mygdx/game/client/initialize/MapInitializer.java
+++ core/src/com/mygdx/game/client/initialize/MapInitializer.java
@@ -5,25 +5,38 @@ import com.mygdx.game.assets.GameScreenAssets;
 import com.mygdx.game.client.ecs.entityfactory.FieldFactory;
 import com.mygdx.game.client.model.Coordinates;
 import com.mygdx.game.config.FieldConfig;
+import lombok.NonNull;
 
+import javax.inject.Inject;
 import java.util.HashMap;
 import java.util.Map;
 
-public final class MapInitializer {
+public class MapInitializer {
 
-  private MapInitializer() {
+  private final FieldFactory fieldFactory;
+  private final GameScreenAssets assets;
+
+  public static final int INITIAL_WIDTH = 10;
+  public static final int INITIAL_HEIGHT = 10;
+
+  @Inject
+  public MapInitializer(@NonNull FieldFactory fieldFactory,
+                        @NonNull GameScreenAssets assets) {
+    this.fieldFactory = fieldFactory;
+    this.assets = assets;
   }
 
-  public static Map<Coordinates, Entity> initializeMap(FieldFactory fieldFactory, GameScreenAssets assets) {
-    return initializeMap(InitialParameters.INITIAL_WIDTH, InitialParameters.INITIAL_HEIGHT, fieldFactory, assets);
+  public Map<Coordinates, Entity> initializeMap() {
+    return initializeMap(INITIAL_WIDTH, INITIAL_HEIGHT);
   }
 
-  public static Map<Coordinates, Entity> initializeMap(int width, int height, FieldFactory fieldFactory, GameScreenAssets assets) {
+  public Map<Coordinates, Entity> initializeMap(int width, int height) {
     var fieldList = new HashMap<Coordinates, Entity>();
     for (int i = 0; i < width; i++) {
       for (int j = 0; j < height; j++) {
         var coords = new Coordinates(i, j);
-        var fieldEntity = fieldFactory.createEntity(assets.getGameConfigs().getAny(FieldConfig.class), coords);
+        var anyConfig = assets.getGameConfigs().getAny(FieldConfig.class);
+        var fieldEntity = fieldFactory.createEntity(anyConfig, coords);
         fieldList.put(coords, fieldEntity);
       }
     }
