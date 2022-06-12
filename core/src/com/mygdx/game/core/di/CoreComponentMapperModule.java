c6c20f31885c24446681167e74caad724e7ef102
Kacper Jankowski <kjankowski@student.agh.edu.pl>
2022-06-12 16:50:35 +0200

    #40 Change how the ComponentMappers are instantiated

--- core/src/com/mygdx/game/core/di/CoreComponentMapperModule.java
+++ core/src/com/mygdx/game/core/di/CoreComponentMapperModule.java
@@ -8,33 +8,28 @@ import com.mygdx.game.core.ecs.component.Slot;
 import com.mygdx.game.core.ecs.component.Stats;
 import dagger.Module;
 import dagger.Provides;
-import dagger.Reusable;
 import lombok.extern.java.Log;
 
 @Module
 @Log
 public class CoreComponentMapperModule {
   @Provides
-  @Reusable
   public ComponentMapper<Name> nameComponentMapper(World world) {
-    return new ComponentMapper<>(Name.class, world);
+    return world.getMapper(Name.class);
   }
 
   @Provides
-  @Reusable
   public ComponentMapper<Position> positionComponentMapper(World world) {
-    return new ComponentMapper<>(Position.class, world);
+    return world.getMapper(Position.class);
   }
 
   @Provides
-  @Reusable
   public ComponentMapper<Slot> slotComponentMapper(World world) {
-    return new ComponentMapper<>(Slot.class, world);
+    return world.getMapper(Slot.class);
   }
 
   @Provides
-  @Reusable
   public ComponentMapper<Stats> statsComponentMapper(World world) {
-    return new ComponentMapper<>(Stats.class, world);
+    return world.getMapper(Stats.class);
   }
 }
