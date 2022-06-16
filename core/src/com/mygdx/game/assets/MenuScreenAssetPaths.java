302738bac8bfbd253fa380d2e178610c72d85cdf
Kacper Jankowski <kjankowski@student.agh.edu.pl>
2022-06-16 09:56:08 +0200

    #61 Fix run configuration

--- core/src/com/mygdx/game/assets/MenuScreenAssetPaths.java
+++ core/src/com/mygdx/game/assets/MenuScreenAssetPaths.java
@@ -2,7 +2,7 @@ package com.mygdx.game.assets;
 
 public final class MenuScreenAssetPaths {
 
-  public static final String ASSETS_PATH = "./assets/";
+  public static final String ASSETS_PATH = "./";
   public static final String TEXTURE_DIR = ASSETS_PATH + "textures/";
   public static final String PLANET = TEXTURE_DIR + "planet.png";
   public static final String NEBULA = TEXTURE_DIR + "noise.png";
