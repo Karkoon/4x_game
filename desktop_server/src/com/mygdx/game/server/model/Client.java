afa52bf059a311fc4516bd831d490bae6102136e
Kacper Jankowski <kjankowski@student.agh.edu.pl>
2022-08-19 21:36:58 +0200

    #63 Add GameRoom field to Client

--- desktop_server/src/com/mygdx/game/server/model/Client.java
+++ desktop_server/src/com/mygdx/game/server/model/Client.java
@@ -6,6 +6,7 @@ import lombok.EqualsAndHashCode.Exclude;
 
 @Data
 public class Client {
+  @Exclude private GameRoom gameRoom;
   @Exclude private final ServerWebSocket socket;
   @Exclude private String playerUsername;
   @Exclude private String playerToken;
