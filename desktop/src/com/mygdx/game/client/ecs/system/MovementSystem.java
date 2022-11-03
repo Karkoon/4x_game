51d8a8115753a2b260d78f1879cc3fda92a06027
Szomeszp <przemkol@onet.eu>
2022-11-03 16:28:38 +0100

    Movement on field occupied by enemy is blocked. WarningDialog instead of OutOfRangeDialog, FieldBlockedDialog etc...

--- desktop/src/com/mygdx/game/client/ecs/system/MovementSystem.java
+++ desktop/src/com/mygdx/game/client/ecs/system/MovementSystem.java
@@ -3,17 +3,22 @@ package com.mygdx.game.client.ecs.system;
 import com.artemis.ComponentMapper;
 import com.artemis.annotations.All;
 import com.artemis.systems.IteratingSystem;
+import com.artemis.annotations.AspectDescriptor;
+import com.artemis.EntitySubscription;
 import com.mygdx.game.client.ecs.component.Highlighted;
 import com.mygdx.game.client.model.ChosenEntity;
 import com.mygdx.game.client.ui.OutOfMoveRangeDialogFactory;
+import com.mygdx.game.client.ui.WarningDialogFactory;
 import com.mygdx.game.client_core.di.gameinstance.GameInstanceScope;
 import com.mygdx.game.client_core.ecs.component.Movable;
+import com.mygdx.game.client_core.model.GameState;
 import com.mygdx.game.client_core.network.service.MoveEntityService;
 import com.mygdx.game.core.ecs.component.Coordinates;
 import com.mygdx.game.core.ecs.component.Field;
 import com.mygdx.game.core.ecs.component.Owner;
 import com.mygdx.game.core.ecs.component.Stats;
 import com.mygdx.game.core.util.DistanceUtil;
+import lombok.NonNull;
 import lombok.extern.java.Log;
 
 import javax.inject.Inject;
@@ -25,36 +30,48 @@ public class MovementSystem extends IteratingSystem {
 
   private final ChosenEntity chosenEntity;
   private final MoveEntityService moveEntityService;
-  private final OutOfMoveRangeDialogFactory moveRangeDialog;
+  private final WarningDialogFactory warningDialog;
   private ComponentMapper<Highlighted> highlightedMapper;
   private ComponentMapper<Coordinates> coordinatesMapper;
   private ComponentMapper<Stats> statsMapper;
   private ComponentMapper<Field> fieldMapper;
   private ComponentMapper<Owner> ownerComponentMapper;
 
+  @AspectDescriptor(all = {Movable.class, Owner.class, Coordinates.class})
+  private EntitySubscription units;
+
   @Inject
   public MovementSystem(
       ChosenEntity chosenEntity,
       MoveEntityService moveEntityService,
-      OutOfMoveRangeDialogFactory moveRangeDialog
+      WarningDialogFactory warningDialog
   ) {
     this.chosenEntity = chosenEntity;
     this.moveEntityService = moveEntityService;
-    this.moveRangeDialog = moveRangeDialog;
+    this.warningDialog = warningDialog;
   }
 
   @Override
   protected void process(int entityId) {
     if (chosenEntity.isAnyChosen() && fieldMapper.has(chosenEntity.peek())) {
-      if (!(ownerComponentMapper.get(chosenEntity.peek()) == null) &&
-              !ownerComponentMapper.get(chosenEntity.peek()).getToken()
-              .equals(ownerComponentMapper.get(entityId).getToken())){
-        log.info("Cannot move on enemy's field!");
+      var chosenEntityId = chosenEntity.pop();
+
+      boolean canMove = true;
+      for (int i = 0; i < units.getEntities().size(); i++) {
+        var entity = units.getEntities().get(i);
+        if (!coordinatesMapper.get(entity).equals(coordinatesMapper.get(chosenEntityId))){
+          continue;
+        }
+        if (!ownerComponentMapper.get(entity).getToken()
+                .equals(ownerComponentMapper.get(entityId).getToken())) {
+          canMove = false;
+          log.info("Cannot move on enemy's field!");
+        }
       }
-      else {
+      if (canMove) {
         log.info("some are chosen and there's a movable highlighted entity");
 
-        var targetCoordinate = coordinatesMapper.get(chosenEntity.pop());
+        var targetCoordinate = coordinatesMapper.get(chosenEntityId);
         var currentCoordinate = coordinatesMapper.get(entityId);
 
         var distance = DistanceUtil.distance(currentCoordinate, targetCoordinate);
@@ -63,12 +80,18 @@ public class MovementSystem extends IteratingSystem {
         log.info("Chce przejsc " + distance + " ale zostało mi " + range);
 
         if (distance > range) {
-          moveRangeDialog.createAndShow(range, distance);
+          warningDialog.createAndShow("Movement out of range!",
+                  "You have " + range + " move points left!" + "You can't move " + distance + " units!");
         } else {
           moveEntityService.moveEntity(entityId, targetCoordinate);
         }
-        highlightedMapper.remove(entityId);
       }
+      else {
+        warningDialog.createAndShow("Movement blocked by enemy unit!",
+                "You cannot stand on this field! It is already occupied by enemy unit!");
+      }
+
+      highlightedMapper.remove(entityId);
     }
   }
 
