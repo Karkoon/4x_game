9ac56b7004969dde28ff8f3747d111055ead5288
Jakub Klimczak <kuberserk@gmail.com>
2022-12-01 11:11:10 +0100

    #247 show unit and building descriptions

--- core/src/com/mygdx/game/core/model/BuildingType.java
+++ core/src/com/mygdx/game/core/model/BuildingType.java
@@ -2,6 +2,12 @@ package com.mygdx.game.core.model;
 
 public enum BuildingType {
 
-  MATERIALS_BUILDING,
-  RECRUITMENT_BUILDING
+  MATERIALS_BUILDING("Material building"),
+  RECRUITMENT_BUILDING("Recruit building");
+
+  public final String name;
+
+  BuildingType(String name) {
+    this.name = name;
+  }
 }
--- core/src/com/mygdx/game/core/model/ImpactOperation.java
+++ core/src/com/mygdx/game/core/model/ImpactOperation.java
@@ -4,16 +4,21 @@ import java.util.function.BiFunction;
 
 public enum ImpactOperation {
 
-  PLUS(Float::sum),
-  MINUS((x,y) -> x - y),
-  MULTIPLE((x,y) -> x * y),
-  DIVIDE((x,y) -> x / y),
-  NONE((x,y) -> null);
+  PLUS(Float::sum, "+"),
+  MINUS((x,y) -> x - y, "-"),
+  MULTIPLE((x,y) -> x * y, "*"),
+  DIVIDE((x,y) -> x / y, "/"),
+  NONE((x,y) -> null, "");
 
   public final BiFunction<Float, Float, Float> function;
+  public final String name;
 
-  ImpactOperation(BiFunction<Float, Float, Float> function) {
+  ImpactOperation(
+    BiFunction<Float, Float, Float> function,
+    String name
+  ) {
     this.function = function;
+    this.name = name;
   }
 
 }
