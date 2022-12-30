0780159b75004306d6a1806b8df20022870b1820
Kacper Jankowski <kjankowski@student.agh.edu.pl>
2022-12-30 12:23:01 +0100

    #228 Change logo

--- core/src/com/mygdx/game/assets/GameScreenAssetPaths.java
+++ core/src/com/mygdx/game/assets/GameScreenAssetPaths.java
@@ -3,7 +3,6 @@ package com.mygdx.game.assets;
 public final class GameScreenAssetPaths {
 
   public static final String ASSETS_PATH = "./";
-  public static final String DEMO_TEXTURE_PATH = ASSETS_PATH + "badlogic.jpg";
   public static final String MODEL_DIR = ASSETS_PATH + "models/";
   public static final String HIGHLIGHT_MODEL = "basic.g3db";
   public static final String TEXTURE_DIR = ASSETS_PATH + "textures/";
